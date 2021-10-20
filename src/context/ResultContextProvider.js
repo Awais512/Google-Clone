4aaeb193ef234e4677b7e4756a1136cf7227f0aa
Awais512 <raza.awais25@gmail.com>
2021-10-20 13:43:21 +0500

    context, searching and images

---
+++ src/context/ResultContextProvider.js
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const ResultContext = createContext();
+const baseUrl = `https://google-search3.p.rapidapi.com/api/v1`;
+
+export const ResultContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('Js Mastery');
+
+  const getResults = async (type) => {
+    setLoading(true);
+    const res = await fetch(`${baseUrl}${type}`, {
+      method: 'GET',
+      headers: {
+        'x-user-agent': 'desktop',
+        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+        'x-rapidapi-key': '366839ef6fmsh152f9e76012271ep19795bjsn8709b76ca128',
+      },
+    });
+    const data = await res.json();
+
+    setResults(data);
+    setLoading(false);
+  };
+
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, loading }}
+    >
+      {children}
+    </ResultContext.Provider>
+  );
+};
+
+export const useResultContext = () => useContext(ResultContext);
